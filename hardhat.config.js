5a51016ac9a010375cdf58798a308f0424164a2f
yoansalvayre <yoan@eveasoft.com>
2022-06-23 10:09:37 +0200

    gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-
+require("hardhat-gas-reporter");
 
 const { PRIVATE_KEY } = process.env;
 
@@ -26,7 +26,7 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`],
     },
     rinkeby: {
-      url: /* "https://eth-rinkeby.alchemyapi.io/v2/LsdjIhhDskpuzoIgg78Rs7BI3mzbdIZJ", */"https://rinkeby.infura.io/v3/03ce6aff8f9b46b8bb3ebf13b2900c71",
+      url: /* "https://eth-rinkeby.alchemyapi.io/v2/LsdjIhhDskpuzoIgg78Rs7BI3mzbdIZJ", */ "https://rinkeby.infura.io/v3/03ce6aff8f9b46b8bb3ebf13b2900c71",
       accounts: [`0x${PRIVATE_KEY}`],
     },
     ethereum: {
@@ -37,14 +37,17 @@ module.exports = {
       url: "https://speedy-nodes-nyc.moralis.io/efad5f42b02dc2f894f286f8/polygon/mumbai",
       accounts: [`0x${PRIVATE_KEY}`],
     },
-  /*   mumbai: {
+    /*   mumbai: {
       url: process.env.MUMBAI_URL,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }, */
   },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 50,
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  
 };
--- test/testMC.js
+++ test/testMC.js
@@ -21,8 +21,8 @@ const proof = [
   "0x7e951591b808536a6b63045d897d2ab7919bb1c6654b8a7f2c146bf9213e98b4",
   "0x80f350aab5d052efca9baae16c0c4ef705bbda3cb5e386a037ff660c217f9e2f",
 ];
-const root = "0xb8851699b8e764dc99a286fdbc20411bec24a4eb54373cf8e40ce88d60382fc6";
-
+const root =
+  "0xb8851699b8e764dc99a286fdbc20411bec24a4eb54373cf8e40ce88d60382fc6";
 
 describe("TestMC", function () {
   let DContract;
@@ -35,7 +35,7 @@ describe("TestMC", function () {
     DContract = await Contract.deploy("/test");
     price1 = await (await DContract.categories(1))["NFTPrice"];
     price2 = await (await DContract.categories(2))["NFTPrice"];
-    price3 = await (await DContract.categories(3))["NFTPrice"]
+    price3 = await (await DContract.categories(3))["NFTPrice"];
   });
 
   describe("Deployment", function () {
@@ -46,17 +46,29 @@ describe("TestMC", function () {
 
   describe("Giveaways", function () {
     it("Should mint 99 giveaways", async function () {
-      await DContract.mintMultipleByOwner(ethAddressList, [1,2,1,1,1,1,3,2,2]);
+      await DContract.mintMultipleByOwner(
+        ethAddressList,
+        [1, 2, 1, 1, 1, 1, 3, 2, 2]
+      );
       expect(await (await DContract.totalSupply()).toString()).to.equal("9");
-      expect(await (await DContract.categories(1))["counterSupply"]).to.equal(5);
-      expect(await (await DContract.categories(2))["counterSupply"]).to.equal(3);
-      expect(await (await DContract.categories(3))["counterSupply"]).to.equal(1);
+      expect(await (await DContract.categories(1))["counterSupply"]).to.equal(
+        5
+      );
+      expect(await (await DContract.categories(2))["counterSupply"]).to.equal(
+        3
+      );
+      expect(await (await DContract.categories(3))["counterSupply"]).to.equal(
+        1
+      );
       expect(await DContract.NFTcategory(0)).to.equal(1);
       expect(await DContract.NFTcategory(6)).to.equal(3);
     });
 
     it("Should be the right owner", async function () {
-      await DContract.mintMultipleByOwner(ethAddressList, [1,2,1,1,1,1,3,2,2]);
+      await DContract.mintMultipleByOwner(
+        ethAddressList,
+        [1, 2, 1, 1, 1, 1, 3, 2, 2]
+      );
       expect(await DContract.ownerOf(0)).to.equal(ethAddressList[0]);
       expect(await DContract.ownerOf(5)).to.equal(ethAddressList[5]);
       expect(await DContract.ownerOf(8)).to.equal(ethAddressList[8]);
@@ -67,45 +79,74 @@ describe("TestMC", function () {
     describe("Presale", function () {
       beforeEach(async function () {
         await DContract.setIsActive();
-        await DContract.setMerkleRoot( root );
+        await DContract.setMerkleRoot(root);
       });
 
       it("Should fail if Presale is not active", async function () {
         expect(
-          DContract.connect(accounts[0]).presaleMint([1,0,0], proof, { value: await ((price1) * 2).toString(), })).to.be.revertedWith("Presale is not opened yet");
+          DContract.connect(accounts[0]).presaleMint([1, 0, 0], proof, {
+            value: await (price1 * 2).toString(),
+          })
+        ).to.be.revertedWith("Presale is not opened yet");
       });
 
       it("Should fail if not Whitelisted", async function () {
         await DContract.setPresaleActive();
-        await expect(DContract.connect(accounts[1]).presaleMint([1,0,0], proof, {value: await ((price1) * 1).toString(), })).to.be.revertedWith("Not whitelisted");
+        await expect(
+          DContract.connect(accounts[1]).presaleMint([1, 0, 0], proof, {
+            value: await (price1 * 1).toString(),
+          })
+        ).to.be.revertedWith("Not whitelisted");
       });
 
       it("Should mint if Whitelisted", async function () {
         await DContract.setPresaleActive();
-        expect(await DContract.connect(accounts[0]).presaleMint([2,0,0], proof, {value: await ((price1) * 2).toString(),}));
+        expect(
+          await DContract.connect(accounts[0]).presaleMint([2, 0, 0], proof, {
+            value: await (price1 * 2).toString(),
+          })
+        );
         expect(await DContract.ownerOf(0)).to.equal(accounts[0].address);
         expect(await DContract.totalSupply()).to.equal(2);
       });
 
-       it("Should mint multiple differents NFT", async function () {
+      it("Should mint multiple differents NFT", async function () {
         await DContract.setPresaleActive();
-        expect(await DContract.connect(accounts[0]).presaleMint([2,1,1], proof, {value: await ((price1) * 2 + (price2) * 1 + (price3) * 1).toString(),}));
+        expect(
+          await DContract.connect(accounts[0]).presaleMint([2, 1, 1], proof, {
+            value: await (price1 * 2 + price2 * 1 + price3 * 1).toString(),
+          })
+        );
         expect(await DContract.ownerOf(0)).to.equal(accounts[0].address);
         expect(await DContract.totalSupply()).to.equal(4);
-        expect(await (await DContract.categories(1))["counterWhitelistSupply"]).to.equal(2);
-        expect(await (await DContract.categories(2))["counterWhitelistSupply"]).to.equal(1);
-        expect(await (await DContract.categories(3))["counterWhitelistSupply"]).to.equal(1);
+        expect(
+          await (
+            await DContract.categories(1)
+          )["counterWhitelistSupply"]
+        ).to.equal(2);
+        expect(
+          await (
+            await DContract.categories(2)
+          )["counterWhitelistSupply"]
+        ).to.equal(1);
+        expect(
+          await (
+            await DContract.categories(3)
+          )["counterWhitelistSupply"]
+        ).to.equal(1);
         expect(await DContract.NFTcategory(0)).to.equal(1);
         expect(await DContract.NFTcategory(3)).to.equal(3);
       });
 
       it("Should fail if Whitelist supply is reached", async function () {
         await DContract.setPresaleActive();
-        expect(await DContract.connect(accounts[0]).presaleMint([0,0,5], proof, {value: await ((price3) * 5).toString(),})).to.be.revertedWith('the presale max is reached for this nft tier');
+        expect(
+          await DContract.connect(accounts[0]).presaleMint([0, 0, 5], proof, {
+            value: await (price3 * 5).toString(),
+          })
+        ).to.be.revertedWith("the presale max is reached for this nft tier");
       });
-
     });
-
 
   isPublicSale &&
     describe("Public Sale", async function () {
@@ -115,109 +156,193 @@ describe("TestMC", function () {
 
       it("Should fail if Presale is active", async function () {
         await DContract.setPresaleActive();
-        await expect(DContract.connect(accounts[1]).mintNFT([1,0,0], {value: await ((price1) * 2).toString(),})).to.be.revertedWith("Presale is still active");
+        await expect(
+          DContract.connect(accounts[1]).mintNFT([1, 0, 0], {
+            value: await (price1 * 2).toString(),
+          })
+        ).to.be.revertedWith("Presale is still active");
       });
 
       it("Should fail if Ether value sent is not correct", async function () {
-        await expect(DContract.connect(accounts[1]).mintNFT([4,0,0], {value: await ((price1) * 1).toString(),})).to.be.revertedWith("Ether value sent is not correct");
+        await expect(
+          DContract.connect(accounts[1]).mintNFT([4, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        ).to.be.revertedWith("Ether value sent is not correct");
       });
 
       it("Should mint 2 NFT", async function () {
-        expect(await DContract.connect(accounts[1]).mintNFT([2,0,0], {value: await ((price1) * 2).toString(),}));
+        expect(
+          await DContract.connect(accounts[1]).mintNFT([2, 0, 0], {
+            value: await (price1 * 2).toString(),
+          })
+        );
         expect(await DContract.ownerOf(0)).to.equal(accounts[1].address);
         expect(await DContract.ownerOf(1)).to.equal(accounts[1].address);
         expect(await DContract.totalSupply()).to.equal(2);
       });
 
       it("Should mint multiple differents NFT", async function () {
-        expect(await DContract.connect(accounts[0]).mintNFT([2,1,1], {value: await ((price1) * 2 + (price2) * 1 + (price3) * 1).toString(),}));
+        expect(
+          await DContract.connect(accounts[0]).mintNFT([2, 1, 1], {
+            value: await (price1 * 2 + price2 * 1 + price3 * 1).toString(),
+          })
+        );
         expect(await DContract.ownerOf(0)).to.equal(accounts[0].address);
         expect(await DContract.totalSupply()).to.equal(4);
-        expect(await (await DContract.categories(1))["counterSupply"]).to.equal(2);
-        expect(await (await DContract.categories(2))["counterSupply"]).to.equal(1);
-        expect(await (await DContract.categories(3))["counterSupply"]).to.equal(1);
+        expect(await (await DContract.categories(1))["counterSupply"]).to.equal(
+          2
+        );
+        expect(await (await DContract.categories(2))["counterSupply"]).to.equal(
+          1
+        );
+        expect(await (await DContract.categories(3))["counterSupply"]).to.equal(
+          1
+        );
         expect(await DContract.NFTcategory(0)).to.equal(1);
         expect(await DContract.NFTcategory(3)).to.equal(3);
       });
 
-       it("Should fail if supply is reached", async function () {
+      it("Should fail if supply is reached", async function () {
         await DContract.changeSupply([10, 0, 0]);
-        expect(await DContract.connect(accounts[0]).mintNFT([11,0,0], {value: await ((price1) * 11).toString(),})).to.be.revertedWith('the sale max is reached for this nft tier');
+        expect(
+          await DContract.connect(accounts[0]).mintNFT([11, 0, 0], {
+            value: await (price1 * 11).toString(),
+          })
+        ).to.be.revertedWith("the sale max is reached for this nft tier");
       });
 
       it("Should withdraw the money", async function () {
-        expect(await DContract.connect(accounts[1]).mintNFT([1,0,0], {value: await ((price1) * 1).toString(), }));
-        expect(await DContract.connect(accounts[2]).mintNFT([0,1,1], {value: await ((price2) * 1 + (price3) * 1).toString(), }));
-        expect(await DContract.connect(accounts[3]).mintNFT([0,2,0], {value: await ((price2) * 2).toString(), }));
-        expect(await DContract.connect(accounts[4]).mintNFT([1,0,0], {value: await ((price1) * 1).toString(), }));
-        expect(await DContract.connect(accounts[5]).mintNFT([1,0,0], {value: await ((price1) * 1).toString(), }));
-        expect(await DContract.connect(accounts[6]).mintNFT([2,1,1], {value: await ((price1) * 2 + (price2) * 1 + (price3) * 1).toString(),}));
-  
+        expect(
+          await DContract.connect(accounts[1]).mintNFT([1, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        );
+        expect(
+          await DContract.connect(accounts[2]).mintNFT([0, 1, 1], {
+            value: await (price2 * 1 + price3 * 1).toString(),
+          })
+        );
+        expect(
+          await DContract.connect(accounts[3]).mintNFT([0, 2, 0], {
+            value: await (price2 * 2).toString(),
+          })
+        );
+        expect(
+          await DContract.connect(accounts[4]).mintNFT([1, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        );
+        expect(
+          await DContract.connect(accounts[5]).mintNFT([1, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        );
+        expect(
+          await DContract.connect(accounts[6]).mintNFT([2, 1, 1], {
+            value: await (price1 * 2 + price2 * 1 + price3 * 1).toString(),
+          })
+        );
+
         await expect(await DContract.totalSupply()).to.equal(11);
 
-        let ownerBalance =(await provider.getBalance(owner.address)).toString() / 1000000000000000000;
+        let ownerBalance =
+          (await provider.getBalance(owner.address)).toString() /
+          1000000000000000000;
         console.log("owner balance before withdraw:", ownerBalance);
-        await expect(await (await provider.getBalance(DContract.address)).toString()/ 1000000000000000000).to.equal(379);
+        await expect(
+          (await (await provider.getBalance(DContract.address)).toString()) /
+            1000000000000000000
+        ).to.equal(379);
 
         await DContract.connect(owner).withdraw();
 
-        ownerBalance =(await provider.getBalance(owner.address)).toString() / 1000000000000000000;
+        ownerBalance =
+          (await provider.getBalance(owner.address)).toString() /
+          1000000000000000000;
         console.log("owner balance after withdraw:", ownerBalance);
-        await expect(await (await provider.getBalance(DContract.address)).toString()/ 1000000000000000000).to.equal(0);
+        await expect(
+          (await (await provider.getBalance(DContract.address)).toString()) /
+            1000000000000000000
+        ).to.equal(0);
       });
     });
 
-     describe("change supply", function () {
-        it("Should change the NFT supply", async function () {
-        expect(await (await DContract.categories(1))["maxSupply"]).to.equal(299);
-        await DContract.changeSupply([499, 0, 0]);
-        expect(await (await DContract.categories(1))["maxSupply"]).to.equal(499);
+  describe("change supply", function () {
+    it("Should change the NFT supply", async function () {
+      expect(await (await DContract.categories(1))["maxSupply"]).to.equal(299);
+      await DContract.changeSupply([499, 0, 0]);
+      expect(await (await DContract.categories(1))["maxSupply"]).to.equal(499);
     });
 
-        it("Should change multiple NFT supplies", async function () {
-        expect(await (await DContract.categories(1))["maxSupply"]).to.equal(299);
-        expect(await (await DContract.categories(3))["maxSupply"]).to.equal(99);
-        await DContract.changeSupply([499, 0, 299]);
-        expect(await (await DContract.categories(1))["maxSupply"]).to.equal(499);
-        expect(await (await DContract.categories(3))["maxSupply"]).to.equal(299);
-        });
-     });
+    it("Should change multiple NFT supplies", async function () {
+      expect(await (await DContract.categories(1))["maxSupply"]).to.equal(299);
+      expect(await (await DContract.categories(3))["maxSupply"]).to.equal(99);
+      await DContract.changeSupply([499, 0, 299]);
+      expect(await (await DContract.categories(1))["maxSupply"]).to.equal(499);
+      expect(await (await DContract.categories(3))["maxSupply"]).to.equal(299);
+    });
+  });
 
-      describe("change price", function () {
-        it("Should change the NFT price", async function () {
-        expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(ethers.utils.parseEther("11"));
-        await DContract.changePrice([ethers.utils.parseEther("1"), 0, 0]);
-        expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(ethers.utils.parseEther("1"));
+  describe("change price", function () {
+    it("Should change the NFT price", async function () {
+      expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("11")
+      );
+      await DContract.changePrice([ethers.utils.parseEther("1"), 0, 0]);
+      expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("1")
+      );
     });
 
-        it("Should change multiple NFT supplies", async function () {
-        expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(ethers.utils.parseEther("11"));
-        expect(await (await DContract.categories(3))["NFTPrice"]).to.equal(ethers.utils.parseEther("98"));
-        await DContract.changePrice([ethers.utils.parseEther("1"), 0, ethers.utils.parseEther("50")]);
-        expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(ethers.utils.parseEther("1"));
-        expect(await (await DContract.categories(3))["NFTPrice"]).to.equal(ethers.utils.parseEther("50"));
-        });
-     });
-
-
+    it("Should change multiple NFT supplies", async function () {
+      expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("11")
+      );
+      expect(await (await DContract.categories(3))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("98")
+      );
+      await DContract.changePrice([
+        ethers.utils.parseEther("1"),
+        0,
+        ethers.utils.parseEther("50"),
+      ]);
+      expect(await (await DContract.categories(1))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("1")
+      );
+      expect(await (await DContract.categories(3))["NFTPrice"]).to.equal(
+        ethers.utils.parseEther("50")
+      );
+    });
+  });
 
   describe("Token URI", function () {
     it("Should set the right URI", async function () {
       await DContract.setIsActive();
-      await expect(await DContract.connect(accounts[1]).mintNFT([1,0,0], {value: await ((price1) * 1).toString(), }));
-      await expect(await DContract.connect(accounts[1]).mintNFT([0,1,1], {value: await ((price2) * 1 + (price3) * 1).toString(), }));
+      await expect(
+        await DContract.connect(accounts[1]).mintNFT([1, 0, 0], {
+          value: await (price1 * 1).toString(),
+        })
+      );
+      await expect(
+        await DContract.connect(accounts[1]).mintNFT([0, 1, 1], {
+          value: await (price2 * 1 + price3 * 1).toString(),
+        })
+      );
       await DContract.setBaseUri("https://test");
       expect(await DContract.tokenURI(0)).to.equal("https://testhidden1.json");
       expect(await DContract.tokenURI(1)).to.equal("https://testhidden2.json");
       expect(await DContract.tokenURI(2)).to.equal("https://testhidden3.json");
       await DContract.setRevealCollection();
-       expect(await DContract.tokenURI(0)).to.equal("https://test0.json");
-       expect(await DContract.tokenURI(1)).to.equal("https://test1.json");
-       expect(await DContract.tokenURI(2)).to.equal("https://test2.json");
+      expect(await DContract.tokenURI(0)).to.equal("https://test0.json");
+      expect(await DContract.tokenURI(1)).to.equal("https://test1.json");
+      expect(await DContract.tokenURI(2)).to.equal("https://test2.json");
     });
 
     it("Should fail for non existing token", async function () {
-      await expect(DContract.tokenURI(0)).to.be.revertedWith("URI query for nonexistent token");
+      await expect(DContract.tokenURI(0)).to.be.revertedWith(
+        "URI query for nonexistent token"
+      );
     });
   });
 });
