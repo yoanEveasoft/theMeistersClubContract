f85536e2bf9280a9d5c06c2db474266eccca52e6
yoansalvayre <yoan@eveasoft.com>
2022-07-11 15:57:23 +0200

    init contract v2

---
+++ contracts/TestMCv2.sol
@@ -0,0 +1,217 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+
+import "hardhat/console.sol";
+
+library StructLib {
+    struct Slot{
+        uint160 sqrtPriceX96;
+        int24 tick;
+        uint16 observationIndex;
+        uint16 observationCardinality;
+        uint16 observationCardinalityNext;
+        uint8 feeProtocol;
+        bool unlocked;
+    }
+}
+
+interface IUniswapPrice {
+    function slot0() view external returns (StructLib.Slot memory);
+}
+
+
+contract TestMCV2 is ERC721A, Ownable  {
+
+    using Strings for uint;
+    using SafeMath for uint256;
+
+    event Staked(address owner, uint256 tokenId, uint256 timeframe);
+    event Unstaked(address owner, uint256 tokenId, uint256 timeframe);
+
+    address public stakingContract;
+    address public poolContract;
+    bytes32 public _root;
+    bool public isActive;
+    bool public isPresaleActive;
+    string private baseURI;
+    string private blindURI;
+    bool private isRevealed = false;
+    uint256 public maxSupply = 5000;
+    uint256 public whitelistSupply = 1000;
+    uint256 public nftPrice = 750 * 10 ** 6; // 750 USDC
+
+
+    // Is staked?
+    mapping(uint256 => bool) public isStaked;
+
+
+    constructor( string memory _baseURI, address _poolContract ) ERC721A("TestMC", "TESTMC") {
+        baseURI = _baseURI;
+        poolContract = _poolContract;
+    }
+
+    modifier isStakingContract {
+        require(msg.sender == stakingContract);
+        _;
+    }
+    modifier isContractPublicSale {
+        require(isActive == true, "Contract is not active");
+        require(isPresaleActive == false, "Presale is still active");
+        _;
+    }
+    modifier isContractPresale {
+        require(isActive == true, "Contract is not active");
+        require(isPresaleActive == true, "Presale is not opened yet");
+        _;
+    }
+
+    function setMerkleRoot(bytes32 root) public onlyOwner {
+        _root = root;
+    }
+
+    function setBaseUri(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function setIsActive() external onlyOwner {
+            isActive = !isActive;
+    }
+
+    function setPresaleActive() external onlyOwner {
+        isPresaleActive = !isPresaleActive;
+    }
+
+    function setRevealCollection() external onlyOwner {
+        isRevealed = true;
+    }
+
+    // Mainnet
+    // function getPrice() public view  returns (uint256) {
+    //     StructLib.Slot memory slot = IUniswapPrice(poolContract).slot0();
+    //     uint256 sqrtPriceX96 = uint256(slot.sqrtPriceX96);
+    //     return sqrtPriceX96;
+    // } 
+
+    // Test
+      function getPrice() public pure  returns (uint256) {
+        uint256 sqrtPriceX96 = 2301336933455729618606400047051907;
+        return sqrtPriceX96;
+    } 
+
+    // Function to change the supply of the selected categories
+    function changeSupply( uint256  newSupply) external onlyOwner{
+                require(newSupply > totalSupply(), "new supply is inferior to actual minted supply");
+                maxSupply = newSupply;
+    }
+
+      // Function to change the supply of the selected categories
+    function changeWhitelistSupply( uint256 newWhitelistSupply) external onlyOwner{
+          require(newWhitelistSupply < maxSupply  , "new supply is inferior to actual minted supply");
+                whitelistSupply = newWhitelistSupply;
+    }
+
+    // Function to change the price of the selected categories
+    function changePrice( uint128 newPrice ) external onlyOwner{
+        nftPrice = newPrice * 10 ** 6;
+    }
+
+    // Function to mint NFTs for giveaway and partnerships
+    function mintByOwner(address _to) public onlyOwner {
+        require(
+            totalSupply() + 1 <= maxSupply,
+            "Tokens number to mint cannot exceed number of MAX tokens category 1"
+        );
+        _safeMint(_to, 1);
+    }
+
+    // Function to mint all NFTs for giveaway and partnerships
+    function mintMultipleByOwner(address[] memory _to) public onlyOwner {
+        require(
+            totalSupply() + _to.length <= maxSupply,
+            "Tokens number to mint cannot exceed number of tokens"
+        );
+        for (uint256 i = 0; i < _to.length; i++) {
+            _safeMint(_to[i], 1); 
+        }
+    }
+
+    // function for presale mint (whitelist)
+    function presaleMint(
+       uint256  _numOfTokens,
+       bytes32[] calldata _proof
+    ) external payable  isContractPresale {
+       require(MerkleProof.verify(_proof, _root, keccak256(abi.encode(msg.sender))), "Not whitelisted");
+        require((whitelistSupply > totalSupply() + _numOfTokens), "the presale max is reached");
+        uint256 totalPrice = nftPrice * _numOfTokens ;
+        uint256 ethInUSDC = ((10 ** 12) * (2 ** 192)) / (getPrice() ** 2);
+         require(
+            totalPrice <= msg.value * ethInUSDC,
+            "Ether value sent is not correct"
+        ); 
+        _safeMint(msg.sender, _numOfTokens);
+    }
+
+    // function for regular mint
+   function mintNFT(uint256 _numOfTokens ) public payable isContractPublicSale {
+       require((maxSupply > totalSupply() + _numOfTokens), "the sale max is reached");
+        uint256 totalPrice = nftPrice * _numOfTokens ;
+        uint256 ethInUSDC = ((10 ** 12) * (2 ** 192)) / (getPrice() ** 2);
+        require(
+            totalPrice <= msg.value * ethInUSDC,
+            "Ether value sent is not correct"
+        ); 
+        _safeMint(msg.sender, _numOfTokens);
+      
+   }
+
+    // Stake function, to stake ur function
+    function stakeNFT(uint256 tokenId) external isStakingContract {
+        isStaked[tokenId] = true;
+        emit Staked(msg.sender, tokenId, block.timestamp);
+    }
+
+    // Unstake funtion, to unstake ur function
+    function unstakeNFT(uint256 tokenId) external isStakingContract {
+        isStaked[tokenId] = false;
+        emit Unstaked(msg.sender, tokenId, block.timestamp);
+    }
+
+
+    // Set the NFT not available for transfer if it's staked
+    function _beforeTokenTransfers(address from,
+        address to,
+        uint256 startTokenId,
+        uint256 quantity) internal override { 
+        super._beforeTokenTransfers(from, to, startTokenId, quantity);
+        require(isStaked[startTokenId] == false, "your NFT is not available for transfer");
+    }
+
+    // tokenURI is the link to the metadatas
+    function tokenURI(uint _tokenId) public view virtual override returns (string memory) {
+         require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+      if (!isRevealed) {
+            return string(abi.encodePacked(blindURI));
+        } else {
+            return string(abi.encodePacked(baseURI, _tokenId.toString()));
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success, "!transfer");
+    }
+}
---
+++ test/testMC2.js
@@ -0,0 +1,325 @@
+const { expect } = require("chai");
+const { waffle, ethers } = require("hardhat");
+const provider = waffle.provider;
+const { defaultAbiCoder, keccak256, parseEther } = require("ethers/lib/utils");
+const { MerkleTree } = require("merkletreejs");
+const ethAddressList = require("../data/addressList");
+
+//paramètres
+const isPresale = true;
+const isPublicSale = true;
+const pool = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
+
+async function forkNetwork(blockNumber = 15082955) {
+  /// Use mainnet fork as provider
+  return network.provider.request({
+    method: "hardhat_reset",
+    params: [
+      {
+        forking: {
+          jsonRpcUrl:
+            "https://eth-mainnet.alchemyapi.io/v2/GQuvG_4t-tC23bv60y0LCF7tS9Zj3eq-",
+          blockNumber: blockNumber,
+        },
+      },
+    ],
+  });
+}
+
+describe("TestMCV2", function () {
+  let Contract;
+  forkNetwork();
+
+  beforeEach(async function () {
+    users = await ethers.getSigners();
+    await network.provider.send("hardhat_setBalance", [
+      users[0].address,
+      "0x1000000000000000000000000000",
+    ]);
+    await network.provider.send("hardhat_setBalance", [
+      users[1].address,
+      "0x1000000000000000000000000000",
+    ]);
+    await network.provider.send("hardhat_setBalance", [
+      users[2].address,
+      "0x1000000000000000000000000000",
+    ]);
+
+    const contract = await ethers.getContractFactory("TestMCV2");
+    Contract = await contract.deploy("/test", pool);
+    price = await Contract.nftPrice();
+
+    sqrtPriceX96 = await Contract.getPrice();
+    preMarginEthInUSDC = (10 ** 12 * 2 ** 192) / sqrtPriceX96 ** 2;
+    ethInUSDC = preMarginEthInUSDC * 0.98;
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right users[0]", async function () {
+      expect(await Contract.owner()).to.equal(users[0].address);
+    });
+  });
+
+  describe("Giveaways", function () {
+    it("Should mint 10 giveaways", async function () {
+      await Contract.mintMultipleByOwner(ethAddressList);
+      expect(await (await Contract.totalSupply()).toString()).to.equal("10");
+    });
+
+    it("Should be the right users[0]", async function () {
+      await Contract.mintMultipleByOwner(ethAddressList);
+      expect(await Contract.ownerOf(0)).to.equal(ethAddressList[0]);
+      expect(await Contract.ownerOf(5)).to.equal(ethAddressList[5]);
+      expect(await Contract.ownerOf(8)).to.equal(ethAddressList[8]);
+    });
+  });
+
+  isPresale &&
+    describe("Presale", function () {
+      let merkleTree;
+      let leaves;
+      let proof;
+      const getProof = (i) => merkleTree.getHexProof(keccak256(leaves[i]));
+
+      beforeEach(async function () {
+        leaves = ethAddressList.map((addr) =>
+          defaultAbiCoder.encode(["address"], [addr])
+        );
+
+        merkleTree = new MerkleTree(leaves, keccak256, {
+          hashLeaves: true,
+          sortPairs: true,
+        });
+
+        await Contract.setMerkleRoot(merkleTree.getHexRoot());
+        proof = merkleTree.getHexProof(keccak256(leaves[1]));
+        await Contract.setIsActive();
+      });
+
+      it("Should fail if Presale is not active", async function () {
+        expect(
+          Contract.connect(users[3]).presaleMint(1, proof, {
+            value: await price.toString(),
+          })
+        ).to.be.revertedWith("Presale is not opened yet");
+      });
+
+      it("Should fail if not Whitelisted", async function () {
+        await Contract.setPresaleActive();
+        expect(
+          Contract.presaleMint(1, proof, {
+            value: await price.toString(),
+          })
+        ).to.be.revertedWith("Not whitelisted");
+      });
+
+      it("Should mint if Whitelisted", async function () {
+        await Contract.setPresaleActive();
+        expect(
+          await Contract.presaleMint(2, proof, {
+            value: await ethers.utils.parseEther("" + (price * 2) / ethInUSDC),
+          })
+        );
+        expect(await Contract.ownerOf(0)).to.equal(users[0].address);
+        expect(await Contract.totalSupply()).to.equal(2);
+      });
+
+      it("Should fail if Whitelist supply is reached", async function () {
+        await Contract.setPresaleActive();
+        await Contract.changeWhitelistSupply(20);
+        expect(
+          await Contract.connect(users[0]).presaleMint(21, proof, {
+            value: await ethers.utils.parseEther("" + (price * 21) / ethInUSDC),
+          })
+        ).to.be.revertedWith("the presale max is reached");
+      });
+    });
+
+  /*   isPublicSale &&
+    describe("Public Sale", async function () {
+      beforeEach(async function () {
+        await Contract.setIsActive();
+      });
+
+      it("Should fail if Presale is active", async function () {
+        await Contract.setPresaleActive();
+        await expect(
+          Contract.connect(users[1]).mintNFT([1, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        ).to.be.revertedWith("Presale is still active");
+      });
+
+      it("Should fail if Ether value sent is not correct", async function () {
+        await expect(
+          Contract.connect(users[1]).mintNFT([4, 0, 0], {
+            value: await (price1 * 1).toString(),
+          })
+        ).to.be.revertedWith("Ether value sent is not correct");
+      });
+
+      it("Should mint 2 NFT", async function () {
+        expect(
+          await Contract.connect(users[1]).mintNFT([2, 0, 0], {
+            value: await ethers.utils.parseEther("" + (price1 * 2) / ethInUSDC),
+          })
+        );
+        expect(await Contract.ownerOf(0)).to.equal(users[1].address);
+        expect(await Contract.ownerOf(1)).to.equal(users[1].address);
+        expect(await Contract.totalSupply()).to.equal(2);
+      });
+
+      it("Should mint multiple differents NFT", async function () {
+        expect(
+          await Contract.connect(users[0]).mintNFT([2, 1, 1], {
+            value: await ethers.utils.parseEther(
+              "" + (price1 * 2 + price2 * 1 + price3 * 1) / ethInUSDC
+            ),
+          })
+        );
+        expect(await Contract.ownerOf(0)).to.equal(users[0].address);
+        expect(await Contract.totalSupply()).to.equal(4);
+        expect(await (await Contract.categories(1))["counterSupply"]).to.equal(
+          2
+        );
+        expect(await (await Contract.categories(2))["counterSupply"]).to.equal(
+          1
+        );
+        expect(await (await Contract.categories(3))["counterSupply"]).to.equal(
+          1
+        );
+        expect(await Contract.NFTcategory(0)).to.equal(1);
+        expect(await Contract.NFTcategory(3)).to.equal(3);
+      });
+
+      it("Should fail if supply is reached", async function () {
+        await Contract.changeSupply([10, 0, 0]);
+        await network.provider.send("evm_increaseTime", [90]);
+        await network.provider.send("evm_mine");
+        expect(
+          await Contract.connect(users[0]).mintNFT([11, 0, 0], {
+            value: await ethers.utils.parseEther(
+              "" + (price1 * 11) / ethInUSDC
+            ),
+          })
+        ).to.be.revertedWith("the sale max is reached for this nft tier");
+      });
+
+      it("Should withdraw the money", async function () {
+        expect(
+          await Contract.connect(users[1]).mintNFT([1, 0, 0], {
+            value: await ethers.utils.parseEther("" + (price1 * 1) / ethInUSDC),
+          })
+        );
+        expect(
+          await Contract.connect(users[2]).mintNFT([0, 1, 1], {
+            value: await ethers.utils.parseEther(
+              "" + (price2 * 1 + price3 * 1) / ethInUSDC
+            ),
+          })
+        );
+        expect(
+          await Contract.connect(users[2]).mintNFT([0, 2, 0], {
+            value: await ethers.utils.parseEther("" + (price2 * 2) / ethInUSDC),
+          })
+        );
+        expect(
+          await Contract.connect(users[1]).mintNFT([1, 0, 0], {
+            value: await ethers.utils.parseEther("" + (price1 * 1) / ethInUSDC),
+          })
+        );
+        expect(
+          await Contract.connect(users[1]).mintNFT([1, 0, 0], {
+            value: await ethers.utils.parseEther("" + (price1 * 1) / ethInUSDC),
+          })
+        );
+        expect(
+          await Contract.connect(users[2]).mintNFT([2, 1, 1], {
+            value: await ethers.utils.parseEther(
+              "" + (price1 * 2 + price2 * 1 + price3 * 1) / ethInUSDC
+            ),
+          })
+        );
+
+        await expect(await Contract.totalSupply()).to.equal(11);
+
+        let usersBalance =
+          (await provider.getBalance(users[0].address)).toString() /
+          1000000000000000000;
+        let ContractBalance =
+          (await (await provider.getBalance(Contract.address)).toString()) /
+          1000000000000000000;
+        console.log("users balance before withdraw:", usersBalance);
+        console.log("contract balance before withdraw:", ContractBalance);
+
+        await expect(
+          (await (await provider.getBalance(Contract.address)).toString()) /
+            1000000000000000000
+        ).to.equal(284.9762192277335);
+
+        await Contract.connect(users[0]).withdraw();
+
+        usersBalance =
+          (await provider.getBalance(users[0].address)).toString() /
+          1000000000000000000;
+        console.log("users[0] balance after withdraw:", usersBalance);
+        await expect(
+          (await (await provider.getBalance(Contract.address)).toString()) /
+            1000000000000000000
+        ).to.equal(0);
+      });
+    }); */
+
+  describe("change supply", function () {
+    it("Should change the NFT supply", async function () {
+      expect(await Contract.maxSupply()).to.equal(5000);
+      await Contract.changeSupply(6000);
+      expect(await Contract.maxSupply()).to.equal(6000);
+    });
+
+    it("Should change whitelist supply", async function () {
+      expect(await Contract.whitelistSupply()).to.equal(1000);
+      await Contract.changeWhitelistSupply(500);
+      expect(await Contract.whitelistSupply()).to.equal(500);
+    });
+  });
+
+  describe("change price", function () {
+    it("Should change the NFT price", async function () {
+      console.log(await Contract.nftPrice());
+      expect(await Contract.nftPrice()).to.equal(750 * 10 ** 6);
+      await Contract.changePrice(1000);
+      expect(await Contract.nftPrice()).to.equal(1000 * 10 ** 6);
+    });
+  });
+
+  describe("Token URI", function () {
+    it("Should set the right URI", async function () {
+      await Contract.setIsActive();
+      await expect(
+        await Contract.connect(users[1]).mintNFT(1, {
+          value: await ethers.utils.parseEther("" + (price * 1) / ethInUSDC),
+        })
+      );
+      await expect(
+        await Contract.connect(users[1]).mintNFT(2, {
+          value: await ethers.utils.parseEther("" + (price * 2) / ethInUSDC),
+        })
+      );
+      await Contract.setBaseUri("https://test");
+      expect(await Contract.tokenURI(0)).to.equal("https://testhidden1.json");
+      expect(await Contract.tokenURI(1)).to.equal("https://testhidden2.json");
+      expect(await Contract.tokenURI(2)).to.equal("https://testhidden3.json");
+      await Contract.setRevealCollection();
+      expect(await Contract.tokenURI(0)).to.equal("https://test0.json");
+      expect(await Contract.tokenURI(1)).to.equal("https://test1.json");
+      expect(await Contract.tokenURI(2)).to.equal("https://test2.json");
+    });
+
+    it("Should fail for non existing token", async function () {
+      await expect(Contract.tokenURI(0)).to.be.revertedWith(
+        "ERC721Metadata: URI query for nonexistent token"
+      );
+    });
+  });
+});
